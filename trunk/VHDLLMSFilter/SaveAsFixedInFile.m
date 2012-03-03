38a665854c72eed22784ef80b8a0e7272c7c732e
bjergekim@gmail.com <bjergekim@gmail.com@7c28a9ba-2a0a-bb32-f8f0-ced7313828c3>
2012-03-03 21:32:36 +0000

    Updated test bench for audiofilter with simulation of NoiseSignal and Noise input data - tested in modelSim for audiofilter_tb and MATLAB with plotAudiofilter.m

---
+++ trunk/VHDLLMSFilter/SaveAsFixedInFile.m
@@ -0,0 +1,14 @@
+function [] = SaveAsFixedInFile(h, name)
+%% Converts vector h to 1.23 and save in file given by name
+% Convert input vector to 1.23 fixed point format 
+h24 = h*2^15;
+
+% Modified for Matlab 7
+fid = fopen(name, 'w');
+for i=1:length(h24)
+    xtext = num2str(round(h24(i)));
+    fprintf(fid, '%s\n', xtext);
+end
+fclose(fid);
+
+end
