a274ebf1682eb388a64cc6cee3524a5d1b0c1812
bjergekim@gmail.com <bjergekim@gmail.com@7c28a9ba-2a0a-bb32-f8f0-ced7313828c3>
2012-03-06 18:20:15 +0000

    LMS optimized filter tested in ModelSim

--- VHDLLMSFilter/audiolmsfilterOpt.vhd
+++ VHDLLMSFilter/audiolmsfilterOpt.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
  
 entity audiolmsfilterOpt is
-  generic (filterOrder : natural := 10;
+  generic (filterOrder : natural := 64;
            coefWidth : natural := 24;
            audioWidth : natural := 24);  -- Default values
   port (
@@ -50,16 +50,15 @@ architecture behaviour of audiolmsfilterOpt is
   subtype prod_type is signed(audioWidth+coefWidth-1 downto 0);
   type prod_array_type is array (0 to filterOrder) of prod_type;
   
-  --constant ADPT_STEP : coeff_type := X"000083";  -- Format decimal 131 -  1.15 with 0.004 (float)
-  --constant ADPT_STEP : coeff_type := X"008312";  -- Format decimal 33554 -  1.23 with 0.004 (float)
-  constant CI_ADPT_STEP : coeff_type := X"010624";  -- Format decimal 67108 -  1.23 with 0.008 (float)
+  --constant CI_ADPT_STEP : coeff_type := X"000083";  -- Format decimal 131 -  1.15 with 0.004 (float)
+  constant CI_ADPT_STEP : coeff_type := X"008312";  -- Format decimal 33554 -  1.23 with 0.004 (float)
+  --constant CI_ADPT_STEP : coeff_type := X"010624";  -- Format decimal 67108 -  1.23 with 0.008 (float)
 
   signal adptStep : coeff_type;
   signal coeff   : coeff_array_type;
   --constant const_coeff : coeff_array_type := 
   --(X"000004", X"000008", X"000012", X"000020", X"00002B", X"00002F", X"00002B", X"000020", X"000012", X"000008", X"000004");
   signal tap     : tap_array_type;
-  signal wk_s    : tap_array_type;
   signal prod    : prod_array_type;
 
   -- Build an enumerated type for the state machine
@@ -72,7 +71,7 @@ architecture behaviour of audiolmsfilterOpt is
   signal sound_sample : std_logic_vector(audioWidth-1 downto 0); -- Right channel
   
   signal input_sample : signed(audioWidth-1 downto 0); -- Input sample to LMS filter
-  signal error : tap_type; -- Output result from LMS filter
+  signal output_sample : tap_type; -- Output result from LMS filter
  
 begin  
   
@@ -143,7 +142,7 @@ begin
         if (mute_left = '1') then
           coe_AudioOut_export <= (others => '0');
         else  
-          coe_AudioOut_export <= std_logic_vector(error(audioWidth-1 downto 0));
+          coe_AudioOut_export <= std_logic_vector(output_sample(audioWidth-1 downto 0));
           --coe_AudioOut_export <= noise_sample;     
         end if;
       end if;
@@ -172,7 +171,9 @@ begin
     variable filtered_result : prod_type;
     variable wk_i : signed((2*audioWidth)-1 downto 0);
     variable wk_ii : signed(audioWidth+coefWidth-1 downto 0);
+    variable wk_s : tap_array_type;
     variable tap_no : index_type;
+    variable error : tap_type; -- Output result from LMS filter
   begin
     
     if csi_AudioClk12MHz_reset_n = '0' then
@@ -182,9 +183,10 @@ begin
         coeff(tap_no) <= (others => '0');
         tap(tap_no) <= (others => '0');
         prod(tap_no) <= (others => '0');
-        wk_s(tap_no) <= (others => '0');
+        wk_s(tap_no) := (others => '0');
       end loop;   
-  		  error <= (others => '0');
+  		  error := (others => '0');
+  		  output_sample <= (others => '0');
   		  filter_state <= idle;
       
     elsif rising_edge(csi_AudioClk12MHz_clk) then  -- faling clock edge
@@ -197,47 +199,53 @@ begin
             filter_state <= step1;
 			    end if;
 			 
-		  when step1 =>  
-        -- Direct FIR filter 
-        -- Shift delayline
-        for no in filterOrder downto 1 loop
-          tap(no) <= tap(no - 1);
-        end loop;
-        tap(0) <= input_sample;
-        tap_no := filterOrder;
-        result := (others => '0');
-				filter_state <= step2;
+		    when step1 =>  
+          -- Direct FIR filter 
+          -- Shift delayline
+          for no in filterOrder downto 1 loop
+            tap(no) <= tap(no - 1);
+          end loop;
+          tap(0) <= input_sample;
+          tap_no := filterOrder;
+          result := (others => '0');
+				  filter_state <= step2;
 
-			when step2 =>  
-        -- Direct FIR filter 
-        -- Performs MAC for FIR filter
-        result := (coeff(tap_no) * tap(tap_no)) + result;
-        if (tap_no = 0) then 
-			  	  filter_state <= step3;
-				else
-           tap_no := tap_no - 1;
-				end if;
+			 when step2 =>  
+          -- Direct FIR filter 
+          -- Performs MAC for FIR filter
+          result := (coeff(tap_no) * tap(tap_no)) + result;
+          if (tap_no = 0) then 
+			  	    filter_state <= step3;
+			 	  else
+             tap_no := tap_no - 1;
+				  end if;
    
-			when step3 =>  
-        filtered_result := shift_right(result, audioWidth-1); 
-        --error := shift_right(signed(sound_sample), 1) - resize(filtered_result, audioWidth);
-        error <= signed(sound_sample) - resize(filtered_result, audioWidth);
-				filter_state <= step4;
+			 when step3 =>  
+			    -- Computes error
+          filtered_result := shift_right(result, audioWidth-1); 
+          --error := shift_right(signed(sound_sample), 1) - resize(filtered_result, audioWidth);
+          error := signed(sound_sample) - resize(filtered_result, audioWidth);
+          tap_no := filterOrder;
+				  filter_state <= step4;
 
-			when step4 =>  
-        -- Performs adjust LMS algorithm of weights, 2 stages pipelining        
-        for tap_no in filterOrder downto 0 loop
+			 when step4 =>  
+          -- Performs adjust LMS algorithm of weights, 2 stages pipelining        
           wk_i := error * tap(tap_no);
-          wk_s(tap_no) <= resize(shift_right(wk_i, audioWidth-1), audioWidth); -- First pipeline (product+shift)
+          wk_s(tap_no) := resize(shift_right(wk_i, audioWidth-1), audioWidth); -- First pipeline (product+shift)
           wk_ii := adptStep * wk_s(tap_no);
           coeff(tap_no) <= coeff(tap_no) + resize(shift_right(wk_ii, audioWidth-1), coefWidth); -- Second pipeline (MAC+shift)
-        end loop;
-				filter_state <= idle;
+          if (tap_no = 0) then
+            filter_state <= idle;
+          else
+            tap_no := tap_no - 1;
+          end if;  
 
-	    when others =>
-				filter_state <= idle;
+	      when others =>
+				  filter_state <= idle;
 					
       end case;	
+			
+			output_sample <= error;
 			
     end if;
   end process;
